1a472dc0eaf9fb32ca080424b1d1cc03779aba2a
zich188 <78915934+zich188@users.noreply.github.com>
2021-05-24 12:05:29 +0200

    Twig - 03. Les globales

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale mail à l'initialisation de l'objet Twig
+$mail = 'gogogo@gmail.com';
+$twig->addGlobal('contactEmail', $mail);
